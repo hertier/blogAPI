aa61afb5f77177baa9037a52705eeafa87c91e84
unknown <hertierntare@gmail.com>
2023-03-21 12:28:40 +0200

    API Documentation

--- django_project/settings.py
+++ django_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount", 
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
 ]
@@ -74,6 +75,13 @@ REST_FRAMEWORK = {
     "rest_framework.authentication.SessionAuthentication",
     "rest_framework.authentication.BasicAuthentication",
 ],
+"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to learn about DRF",
+    "VERSION": "1.0.0",
 }
 
 
